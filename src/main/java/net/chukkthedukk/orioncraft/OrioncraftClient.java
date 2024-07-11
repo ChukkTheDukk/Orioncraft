8c7ebb6b46176b7d466a4d48a907c57c927667e5
ChukkTheDukk <ryanmatthewstevenson@gmail.com>
2024-07-11 00:57:58 -0600

    Initial commit

---
+++ src/main/java/net/chukkthedukk/orioncraft/OrioncraftClient.java
@@ -0,0 +1,10 @@
+package net.chukkthedukk.orioncraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OrioncraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/chukkthedukk/orioncraft/OrioncraftDataGenerator.java
@@ -0,0 +1,11 @@
+package net.chukkthedukk.orioncraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OrioncraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
